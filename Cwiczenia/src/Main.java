fab6fd6bcdb7be9e2c73eec07e0202674206e768
Bolomaks <139542089+Bolomaks@users.noreply.github.com>
2026-09-11 13:55:47 +0200

    Add files via upload

---
+++ Cwiczenia/src/Main.java
@@ -0,0 +1,23 @@
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    System.out.print("Hello World"); // sout
+    System.out.println("Cokolwiek");
+
+    // komentarz jednoliniowy (ctrl+/)
+    int calkowita=5;
+    double rzeczywista=1.5;
+
+    System.out.println("Wartość zmiennej \"calkowita\" to " + calkowita);
+    System.out.println("Wartość zmiennej \"calkowita\" po podzieleniu na dwa: " + calkowita/2);
+
+    char znak = 'a';
+    System.out.println("Literka a ma wartość: "+ (znak+0));
+
+    String napis = "napis";
+
+    System.out.println("Wielkie litery: " + napis.toUpperCase());
+
+
+
+}
