0957e236080a04518e0ab36c08a91b6ca85ae6fe
Maks Kordaszewski <test@gmail.com>
2026-09-11 14:30:38 +0200

    Zadanie 4 zrobione

--- Zadanie01/src/Main.java
+++ Zadanie01/src/Main.java
@@ -14,8 +14,12 @@ void main() {
     double liczba = 0.66;
     //zad 3
     System.out.println("Mam na imię "+imie+", mam "+(2026-rok)+" lat  i będę pisać maturę za "+liczba+" roku.");
-    
+
     //zad 4
-
-
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Napisz ile jest stopni na dworze: ");
+    Double stopnie = sc.nextDouble();
+    Double Fahrenheit = stopnie*1.8+32.0;
+    System.out.println("Jest "+Fahrenheit+" stopni fahrenheita");
+    
 }
