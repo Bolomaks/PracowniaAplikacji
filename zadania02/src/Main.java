c5759997661467229584ac77a659ee6fff3d8a5b
Maks Kordaszewski <test@gmail.com>
2026-09-15 09:21:45 +0200

    zrobilem zadanie 2

--- zadania02/src/Main.java
+++ zadania02/src/Main.java
@@ -2,12 +2,32 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
 //zad 1
-    Scanner sc = new Scanner(System.in)
+    Scanner sc = new Scanner(System.in);
     System.out.println("Napisz liczbę: ");
     int liczba = sc.nextInt();
 
     if(liczba % 3 == 0){
         System.out.println("Liczba jest podzielna przez 3");
+    }else {
+        System.out.println("Liczba nie jest podzielna przez 3");
     }
-    
+// zad 2
+    Scanner sc1 = new Scanner(System.in);
+    System.out.println("Napisz pierwsza liczbe: ");
+    int liczba1 = sc1.nextInt();
+    Scanner sc2 = new Scanner(System.in);
+    System.out.println("Napisz druga liczbe: ");
+    int liczba2 = sc2.nextInt();
+    Scanner sc3 = new Scanner(System.in);
+    System.out.println("Napisz trzecia liczbe: ");
+    int liczba3 = sc3.nextInt();
+
+    int wynik1 = liczba1 + liczba2;
+    int wynik2 = liczba1 + liczba3;
+    int wynik3 = liczba2 + liczba3;
+    if(wynik1>liczba3 && wynik2>liczba2 && wynik3>liczba1){
+        System.out.println("Da się zrobic trójkąt");
+    }else{
+        System.out.println("Nie da sie zrobic trójkąta");
+    }
 }
