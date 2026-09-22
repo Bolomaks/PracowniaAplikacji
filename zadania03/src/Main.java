8e7e83ba85c40a61ab69562fab74851a0c9da201
Maks Kordaszewski <email>
2026-09-22 09:04:35 +0200

    Zrobilem zadanie 3

--- zadania03/src/Main.java
+++ zadania03/src/Main.java
@@ -7,20 +7,27 @@ void main() {
     int liczba = sc.nextInt();
     int i = 1;
 
-while(i<=liczba){
-    System.out.println(i);
-    i+=2;
-}
+    while (i <= liczba) {
+        System.out.println(i);
+        i += 2;
+    }
 
 //zad 2
     System.out.println("Podaj liczbe całkowitą dodatnia n: ");
     int n = sc.nextInt();
-    int i1 = 0;
     int wynik = 2;
-    while(wynik<n){
+    while (wynik < n) {
         System.out.println(wynik);
         wynik *= 2;
-        i1++;
     }
 
+    //zad 3
+    System.out.println("Napisz liczbę: ");
+    int liczba1 = sc.nextInt();
+
+    while (liczba1 != 0) {
+        System.out.println("Jeszcze raz");
+        liczba1 = sc.nextInt();
+    }
+    
 }
